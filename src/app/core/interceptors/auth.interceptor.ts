6ba700d4fa43deb6eed1c84a24929fa2d38d71a8
Ignacio Recabarren <86744231+nachoreca@users.noreply.github.com>
2026-04-23 12:39:04 -0300

    Login and register users issues fixed; proxy added for prevent CORS problems

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -1,8 +1,5 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  const authReq = req.clone({
-    withCredentials: true,
-  });
-  return next(authReq);
+  return next(req);
 };
--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -11,31 +11,32 @@ export interface User {
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
-  private readonly apiUrl =
-    'http://127.0.0.1:5001/atom-challenge-a52c7/us-central1/api';
+  // private readonly apiUrl =
+  //   'http://127.0.0.1:5001/atom-challenge-a52c7/us-central1/api';
+  private readonly apiUrl = "/api";
+
 
   constructor(
     private http: HttpClient,
     private router: Router,
   ) {}
 
-  findUserByEmail(email: string): Observable<User | null> {
-    return this.http.get<User | null>(`${this.apiUrl}/users?email=${email}`, {
-      withCredentials: true,
-    });
+  findUserByEmail(email: string): Observable<{ user: User | null }> {
+    return this.http.get<{ user: User | null }>(
+      `${this.apiUrl}/users/${email}`,
+    );
   }
 
   createUser(email: string): Observable<User> {
     return this.http.post<User>(
       `${this.apiUrl}/users`,
       { email },
-      { withCredentials: true },
     );
   }
 
   logout(): void {
     this.http
-      .post(`${this.apiUrl}/auth/logout`, {}, { withCredentials: true })
+      .post(`${this.apiUrl}/auth/logout`, {})
       .subscribe();
     localStorage.removeItem('isLoggedIn');
     this.router.navigate(['/login']);
