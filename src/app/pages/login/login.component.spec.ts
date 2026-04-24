b1a5b9c27da8f18877082b2e337b5197fd67a25e
Ignacio Recabarren <86744231+nachoreca@users.noreply.github.com>
2026-04-24 11:44:14 -0300

    Add unit test for services, app component, login and tasks components,

--- src/app/pages/login/login.component.spec.ts
+++ src/app/pages/login/login.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { LoginComponent } from './login.component';
 
 describe('LoginComponent', () => {
@@ -8,9 +9,8 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LoginComponent]
-    })
-    .compileComponents();
+      imports: [LoginComponent, HttpClientTestingModule, RouterTestingModule],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
@@ -19,5 +19,41 @@ describe('LoginComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have invalid form when empty', () => {
+    expect(component.loginForm.invalid).toBeTrue();
+  });
+
+  it('should have valid form with correct email', () => {
+    component.loginForm.setValue({ email: 'test@test.com' });
+    expect(component.loginForm.valid).toBeTrue();
+  });
+
+  it('should have invalid form with incorrect email', () => {
+    component.loginForm.setValue({ email: 'notanemail' });
+    expect(component.loginForm.invalid).toBeTrue();
+  });
+
+  it('should not submit if form is invalid', () => {
+    spyOn(component as any, 'onSubmit').and.callThrough();
+    component.loginForm.setValue({ email: '' });
+    component.onSubmit();
+    expect(component.isLoading).toBeFalse();
+  });
+
+  it('should show create dialog when pendingEmail is set', () => {
+    component.pendingEmail = 'test@test.com';
+    component.showCreateDialog = true;
+    fixture.detectChanges();
+    expect(component.showCreateDialog).toBeTrue();
+  });
+
+  it('should clear pendingEmail on cancel', () => {
+    component.pendingEmail = 'test@test.com';
+    component.showCreateDialog = true;
+    component.onCancelCreate();
+    expect(component.pendingEmail).toBe('');
+    expect(component.showCreateDialog).toBeFalse();
   });
 });
